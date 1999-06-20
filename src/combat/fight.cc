56de0a8e5c53fa801b9c5b829368ab9fc3dce161
John Rothe <forget@tempusmud.com>
1999-06-20 16:09:14 +0000

    Energy Field - Whoops. I forgot it wasn't supposed to block the attack. :P

--- src/combat/fight.cc
+++ src/combat/fight.cc
@@ -3148,7 +3148,7 @@ damage( struct char_data * ch, struct char_data * victim, int dam,
 			}
 			GET_POS(ch) = POS_SITTING;
 			WAIT_STATE(ch, 2 RL_SEC);
-			DAM_RETURN( TRUE );
+			//DAM_RETURN( TRUE );
 		    }
 		}
 	    }
