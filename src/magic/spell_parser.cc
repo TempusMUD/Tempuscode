7e822d919c1c778fdc9b72b32d48a0db47493380
John Rothe <forget@tempusmud.com>
1999-08-29 17:14:40 +0000

    slightly upped the wait state on symbol of pain

--- src/magic/spell_parser.cc
+++ src/magic/spell_parser.cc
@@ -984,7 +984,9 @@ call_magic(struct char_data * caster, struct char_data * cvict,
 	    same_vict = true;
 	if (mag_damage(level, caster, cvict, spellnum, savetype)) {
 	    if (casttype == CAST_SPELL && !same_vict) {
-		WAIT_STATE(caster, 2 RL_SEC);
+			WAIT_STATE(caster, 2 RL_SEC);
+		if(spellnum == SPELL_SYMBOL_OF_PAIN)
+			WAIT_STATE(caster, 1 RL_SEC);
 		mana = mag_manacost(caster, spellnum);
 		if (mana > 0)
 		    GET_MANA(caster) = 
