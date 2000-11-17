afd4af3194d822b28ad8a28cef8f646ad080b0f1
John Rothe <forget@tempusmud.com>
2000-11-17 02:10:46 +0000

    YACB

--- src/misc/act.informative.cc
+++ src/misc/act.informative.cc
@@ -3129,6 +3129,7 @@ ACMD(do_who)
 			 !strncmp(GET_NAME(tch), "Dogma", 5)    ? " CHIEF " :
 			 !strncmp(GET_NAME(tch), "Stryker", 7)  ? "CODEMAN" :
 			 !strncmp(GET_NAME(tch), "Forget", 6)   ? " CODER " :
+			 !strncmp(GET_NAME(tch), "Hubris", 6)   ? "EC ARCH" :
 			 LEV_ABBR(tch)),  CCYEL_BLD(ch, C_NRM), CCNRM(ch, C_NRM),
 			PRF2_FLAGGED(tch, PRF2_NOWHO) ? nowho_buf : "",
 			CCGRN(ch, C_NRM), 
