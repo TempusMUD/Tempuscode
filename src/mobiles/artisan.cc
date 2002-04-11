5238a0cce9e70f050a644df5597d24f7e76383f2
Daniel Lowe <azimuth@tempusmud.com>
2002-04-11 13:31:27 +0000

    Artisan is now working enough to use in Raza's zone.

--- src/mobiles/artisan.cc
+++ src/mobiles/artisan.cc
@@ -7,25 +7,11 @@
 #include "actions.h"
 #include "char_class.h"
 #include "comm.h"
+#include "tmpstr.h"
+#include "screen.h"
+#include "handler.h"
 
 SPECIAL(artisan);
-
-const int CRAFT_RACE = 0;
-const int CRAFT_CLASS = 1;
-const int CRAFT_ALIGN = 2;
-const int CRAFT_ALL = 4;
-
-const int CRAFT_GOOD = 0;
-const int CRAFT_EVIL = 1;
-const int CRAFT_NEUTRAL = 2;
-
-static int cmd_slap, cmd_smirk, cmd_sayto, cmd_cry;
-
-struct craft_restr {
-	bool match(Creature *ch);
-	int kind;
-	int val;
-};
 
 struct craft_component {
 	short item;
@@ -37,87 +23,48 @@ struct craft_component {
 
 class craft_item {
 	public:
-		bool can_make(Creature *keeper);
+		char *next_requirement(Creature *keeper);
+		obj_data *create(Creature *keeper, Creature *recipient);
 
 		short vnum;	// object to be offered when all components are held
 		long cost; // -1 means to use default cost
+		int fail_pct;
 		vector<craft_component *> required;
 };
 
 struct craftshop {
-		bool accepts(Creature *ch);
-		bool refuses(Creature *ch);
-
 		short room;
 		short keeper_vnum;
-		short clan_id;
-		short clan_tax;
 		vector<craft_item *> items;
-		vector<craft_restr *> accept_list;
-		vector<craft_restr *> refuse_list;
 };
 
+static int cmd_slap, cmd_smirk, cmd_cry;
 vector<craftshop *> shop_list;
-
-bool
-craft_restr::match(Creature *ch)
-{
-	switch (this->kind) {
-		case CRAFT_RACE:
-			if (GET_RACE(ch) == this->val)
-				return true;
-			break;
-		case CRAFT_CLASS:
-			if (GET_CLASS(ch) == this->val)
-				return true;
-			break;
-		case CRAFT_ALIGN:
-			if (IS_GOOD(ch) && this->val == CRAFT_GOOD)
-				return true;
-			if (IS_EVIL(ch) && this->val == CRAFT_EVIL)
-				return true;
-			if (IS_NEUTRAL(ch) && this->val == CRAFT_NEUTRAL)
-				return true;
-			break;
-		case CRAFT_ALL:
-			return true;
-	}
-
-	return false;
-}
-
-bool
-craftshop::accepts(Creature *ch)
-{
-	vector<craft_restr *>::iterator restr_iter;
-
-	restr_iter = accept_list.begin();
-	for (;restr_iter != accept_list.end();restr_iter++)
-		if (restr_iter[0]->match(ch))
-			return true;
-	return false;
-}
-
-bool
-craftshop::refuses(Creature *ch)
-{
-	vector<craft_restr *>::iterator restr_iter;
-
-	restr_iter = refuse_list.begin();
-	for (;restr_iter != refuse_list.end();restr_iter++)
-		if (restr_iter[0]->match(ch))
-			return true;
-	return false;
-}
 
 void
 craft_parse_item(craftshop *shop, xmlNodePtr node)
 {
+	xmlNodePtr sub_node;
 	craft_item *new_item;
+	craft_component *compon;
 	
 	new_item = new craft_item;
 	new_item->vnum = xmlGetIntProp(node, "vnum");
 	new_item->cost = xmlGetIntProp(node, "cost");
+	new_item->fail_pct = xmlGetIntProp(node, "failure", 0);
+	for (sub_node = node->xmlChildrenNode; sub_node; sub_node = sub_node->next) {
+		if (xmlMatches(sub_node->name, "requires")) {
+			compon = new craft_component;
+			compon->item = xmlGetIntProp(sub_node, "item");
+			compon->material = xmlGetIntProp(sub_node, "material");
+			compon->weight = xmlGetIntProp(sub_node, "weight");
+			compon->value = xmlGetIntProp(sub_node, "value");
+			compon->amount = xmlGetIntProp(sub_node, "amount");
+			new_item->required.insert(new_item->required.end(), compon);
+		} else {
+			slog("Invalid XML tag while parsing artisan");
+		}
+	}
 	shop->items.insert(shop->items.end(), new_item);
 	return;
 }
@@ -127,7 +74,6 @@ craftshop_load(xmlNodePtr node)
 {
 	xmlNodePtr sub_node;
 	craftshop *new_shop;
-	craft_restr *new_restr;
 	xmlChar *prop;
 
 	new_shop = new craftshop;
@@ -135,48 +81,10 @@ craftshop_load(xmlNodePtr node)
 	new_shop->keeper_vnum = xmlGetIntProp(node, "keeper");
 	for (sub_node = node->xmlChildrenNode; sub_node; sub_node = sub_node->next) {
 		prop = NULL;
-		if (xmlMatches(sub_node->name, "refuse")) {
-			new_restr = new craft_restr;
-			
-			if ((prop = xmlGetProp(sub_node, (const xmlChar *)"class")) != NULL) {
-				new_restr->kind = CRAFT_CLASS;
-				new_restr->val = search_block((const char *)prop, pc_char_class_types, true);
-				if (new_restr->val < 0)
-					slog("WARNING: refuse set to invalid class");
-				free(prop);
-			} else if ((prop = xmlGetProp(sub_node, (const xmlChar *)"race")) != NULL) {
-				new_restr->kind = CRAFT_RACE;
-				new_restr->val = search_block((const char *)prop, player_race, true);
-				if (new_restr->val < 0)
-					slog("WARNING: refuse set to invalid race");
-				free(prop);
-			} else {
-				slog("SYSERR: RACE and CLASS unspecified in craftshop refusal");
-			}
-
-			new_shop->refuse_list.insert(new_shop->refuse_list.end(), new_restr);
-		} else if (xmlMatches(sub_node->name, "accepts")) {
-			new_restr = new craft_restr;
-			
-			if ((prop = xmlGetProp(sub_node, (const xmlChar *)"class")) != NULL) {
-				new_restr->kind = CRAFT_CLASS;
-				new_restr->val = search_block((const char *)prop, pc_char_class_types, true);
-				if (new_restr->val < 0)
-					slog("WARNING: accept set to invalid class");
-				free(prop);
-			} else if ((prop = xmlGetProp(sub_node, (const xmlChar *)"race")) != NULL) {
-				new_restr->kind = CRAFT_RACE;
-				new_restr->val = search_block((const char *)prop, player_race, true);
-				if (new_restr->val < 0)
-					slog("WARNING: accept set to invalid race");
-				free(prop);
-			} else {
-				slog("SYSERR: RACE and CLASS unspecified in craftshop refusal");
-			}
-
-			new_shop->accept_list.insert(new_shop->accept_list.end(), new_restr);
-		} else if (xmlMatches(sub_node->name, "item")) {
+		if (xmlMatches(sub_node->name, "item")) {
 			craft_parse_item(new_shop, sub_node);
+		} else {
+			slog("Invalid XML tag while parsing artisan");
 		}
 	}
 
@@ -189,31 +97,210 @@ craftshop_find(Creature *keeper)
 	vector<craftshop *>::iterator shop;
 
 	for (shop = shop_list.begin(); shop != shop_list.end(); shop++)
-		if (real_mobile_proto(shop[0]->keeper_vnum) == keeper)
+		if (keeper->mob_specials.shared->vnum == shop[0]->keeper_vnum &&
+				keeper->in_room->number == shop[0]->room)
 			return *shop;
 
 	return NULL;
 }
 
+char *
+craft_item::next_requirement(Creature *keeper)
+{
+	obj_data *obj;
+	vector<craft_component *>::iterator compon;
+
+	for (compon = required.begin();compon != required.end();compon++ ) {
+		// Item components are all we support right now
+		if (compon[0]->item) {
+			obj = get_obj_in_list_num(compon[0]->item, keeper->carrying);
+			if (!obj) {
+				obj = real_object_proto(compon[0]->item);
+				return tmp_strdup(obj->short_description);
+			}
+		} else {
+			slog("SYSERR: Unimplemented requirement in artisan");
+		}
+	}
+
+	return NULL;
+}
+
+obj_data *
+craft_item::create(Creature *keeper, Creature *recipient)
+{
+	obj_data *obj;
+	vector<craft_component *>::iterator compon;
+
+	for (compon = required.begin();compon != required.end();compon++ ) {
+		// Item components are all we support right now
+		if (compon[0]->item) {
+			obj = get_obj_in_list_num(compon[0]->item, keeper->carrying);
+			if (!obj) {
+				slog("SYSERR: craft_remove_prereqs called without all prereqs");
+				return NULL;
+			}
+			obj_from_char(obj);
+			extract_obj(obj);
+		} else {
+			slog("SYSERR: Unimplemented requirement in artisan");
+			return NULL;
+		}
+	}
+
+	if (fail_pct && number(0, 100) < fail_pct)
+		return NULL;
+
+	obj = read_object(vnum);
+	obj_to_char(obj, recipient);
+	return obj;
+}
+
+char *
+list_commission_item(Creature *ch, Creature *keeper, int idx, craft_item *item, char *msg)
+{
+	obj_data *obj;
+	char *needed;
+	char *item_prefix;
+
+	obj = real_object_proto(item->vnum);
+	item_prefix = tmp_sprintf("%3d%s)%s",
+		idx, CCRED(ch, C_NRM), CCNRM(ch, C_NRM));
+	// We have to find out what we need.
+	if (item->next_requirement(keeper))
+		needed = tmp_strcat(CCRED(ch, C_NRM), "Unavailable", CCNRM(ch, C_NRM), NULL);
+	else
+		needed = tmp_strcat(CCGRN(ch, C_NRM), " Available ", CCNRM(ch, C_NRM), NULL);
+	
+	return tmp_sprintf("%s%s  %s %-43s %11ld\r\n", msg,
+		item_prefix,
+		needed,
+		CAP(tmp_strdup(obj->short_description)),
+		item->cost);
+		
+}
+
 void
 craftshop_list(Creature *keeper, Creature *ch)
 {
-	obj_data *obj;
+	craftshop *shop;
+	vector<craft_item *>::iterator item;
+	char *msg = "";
+	int idx = 0;
 
-	if (keeper->carrying) {
-		buf[0] = '\0';
-		for (obj = keeper->carrying;obj;obj = obj->next_content) {
-			if (CAN_SEE_OBJ(ch, obj)) {
-				strcat(buf, obj->short_description);
-				strcat(buf, "\r\n");
+	shop = craftshop_find(keeper);
+
+	if (shop->items.empty()) {
+		msg = tmp_sprintf("%s I'm not in business right now.", GET_NAME(ch));
+		do_say(keeper, msg, 0, SCMD_SAY_TO, NULL);
+		return;
+	}
+
+	msg = tmp_strcat(msg,
+		CCCYN(ch, C_NRM),
+		" ##               Item                                               Cost\r\n-------------------------------------------------------------------------\r\n",
+		CCNRM(ch, C_NRM),
+		NULL);
+	for (item = shop->items.begin();item != shop->items.end();item++) {
+		idx++;
+		msg = list_commission_item(ch, keeper, idx, *item, msg);
+	}
+
+	page_string(ch->desc, msg);
+}
+
+void
+craftshop_buy(craftshop *shop, Creature *keeper, Creature *ch, char *arguments)
+{
+	vector<craft_item *>::iterator item_itr;
+	craft_item *item;
+	obj_data *obj;
+	char *arg, *msg, *needed_str;
+
+	arg = tmp_getword(&arguments);
+
+	item = NULL;
+	if (*arg == '#') {
+		unsigned int num;
+
+		arg++;
+		num = (unsigned int)atoi(arg) - 1;
+		if (num >= 0 && num < shop->items.size())
+			item = shop->items[atoi(arg)];
+	} else {
+		for (item_itr= shop->items.begin();item_itr!= shop->items.end();item_itr++) {
+			if (isname(arg, real_object_proto(item_itr[0]->vnum)->name)) {
+				item = *item_itr;
+				break;
 			}
 		}
-
-		page_string(ch->desc, buf);
-	} else {
-		sprintf(buf, "%s I'm not carrying anything!", GET_NAME(ch));
-		do_gen_comm(keeper, buf, 0, SCMD_SAY_TO, NULL);
 	}
+
+	if (!item) {
+		msg = tmp_sprintf("%s I can't make that!  Use the LIST command!", GET_NAME(ch));
+		do_say(keeper, msg, 0, SCMD_SAY_TO, NULL);
+		return;
+	}
+
+	needed_str = item->next_requirement(keeper);
+	if (needed_str) {
+		msg = tmp_sprintf("%s I don't have the necessary materials.", GET_NAME(ch));
+		do_say(keeper, msg, 0, SCMD_SAY_TO, NULL);
+		msg = tmp_sprintf("%s Give me %s.", GET_NAME(ch), needed_str);
+		do_say(keeper, msg, 0, SCMD_SAY_TO, NULL);
+		return;
+	}
+
+	if (item->cost > GET_GOLD(ch)) {
+		msg = tmp_sprintf("%s You don't have enough money for me to make that.  It costs %ld.", GET_NAME(ch), item->cost);
+		do_say(keeper, msg, 0, SCMD_SAY_TO, NULL);
+		return;
+	}
+
+	if (IS_CARRYING_N(ch) + 1 > CAN_CARRY_N(ch)) {
+		msg = tmp_sprintf("%s You can't carry any more items.", GET_NAME(ch));
+		do_say(keeper, msg, 0, SCMD_SAY_TO, NULL);
+		return;
+	}
+
+	if (IS_CARRYING_W(ch) + real_object_proto(item->vnum)->getWeight() > CAN_CARRY_W(ch)) {
+		msg = tmp_sprintf("%s You can't carry any more weight.", GET_NAME(ch));
+		do_say(keeper, msg, 0, SCMD_SAY_TO, NULL);
+		return;
+	}
+
+	GET_GOLD(ch) -= item->cost;
+	obj = item->create(keeper, ch);
+
+	if (!obj) {
+		msg = tmp_sprintf("%s I am sorry.  I failed to make it and I used up all my materials.", GET_NAME(ch));
+		do_say(keeper, msg, 0, SCMD_SAY_TO, NULL);
+		return;
+	}
+
+	send_to_char(ch, "You buy %s for %ld gold.\r\n", obj->short_description, item->cost);
+	switch (number(0, 20)) {
+		case 0:
+			msg = tmp_strcat(GET_NAME(ch), " Glad to do business with you");
+			break;
+		case 1:
+			msg = tmp_strcat(GET_NAME(ch), " Come back soon!");
+			break;
+		case 2:
+			msg = tmp_strcat(GET_NAME(ch), " Have a nice day.");
+			break;
+		case 3:
+			msg = tmp_strcat(GET_NAME(ch), " Thanks, and come again!");
+			break;
+		case 4:
+			msg = tmp_strcat(GET_NAME(ch), " Nice doing business with you");
+			break;
+		default:
+			msg = NULL;
+			break;
+	}
+	if (msg)
+		do_say(keeper, msg, 0, SCMD_SAY_TO, NULL);
 }
 
 void
@@ -232,16 +319,16 @@ assign_artisans(void)
 
 	cmd_slap = find_command("slap");
 	cmd_smirk = find_command("smirk");
-	cmd_sayto = find_command(">");
 	cmd_cry = find_command("cry");
 }
 
 SPECIAL(artisan)
 {
 	struct Creature *keeper = (Creature *)me;
+	char *msg;
 	craftshop *shop;
 
-	if ( spec_mode != SPECIAL_CMD )
+	if (spec_mode != SPECIAL_CMD)
 		return false;
 
 	if (!ch || !keeper || !AWAKE(keeper))
@@ -250,7 +337,7 @@ SPECIAL(artisan)
 	if (CMD_IS("steal") && GET_LEVEL(ch) < LVL_IMMORT) {
 		do_action(keeper, GET_NAME(ch), cmd_slap, 0);
 		sprintf(buf, "%s is a bloody thief!", GET_NAME(ch));
-		do_gen_comm(keeper, buf, 0, SCMD_SHOUT, NULL);
+		do_say(keeper, buf, 0, SCMD_SHOUT, NULL);
 		return true;
 	}
 
@@ -260,22 +347,29 @@ SPECIAL(artisan)
 	shop = craftshop_find(keeper);
 
 	if (!shop || shop->room != keeper->in_room->number) {
-		sprintf(buf, "%s Sorry!  I don't have my tools!", GET_NAME(ch));
+		msg = tmp_sprintf("%s Sorry!  I don't have my tools!", GET_NAME(ch));
 		do_say(keeper, buf, 0, SCMD_SAY_TO, NULL);
 		do_action(keeper, "", cmd_cry, 0);
 		return true;
 	}
 
+/*
 	if (!shop->accepts(ch) && shop->refuses(ch)) {
 		sprintf(buf, "%s I don't deal with your type.", GET_NAME(ch));
 		do_say(keeper, buf, 0, SCMD_SAY_TO, NULL);
 		do_action(keeper, GET_NAME(ch), cmd_smirk, 0);
 		return true;
 	}
+*/
 
 	if (CMD_IS("list"))
 		craftshop_list(keeper, ch);
-	else
+	else if (CMD_IS("buy"))
+		craftshop_buy(shop, keeper, ch, argument);
+	else if (CMD_IS("sell")) {
+		msg = tmp_sprintf("%s I don't sell things, I make them.", GET_NAME(ch));
+		do_say(keeper, buf, 0, SCMD_SAY_TO, NULL);
+	} else
 		return false;
 	
 	return true;
