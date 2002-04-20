b12c1477a9127e7b5a8bbbdbe014159bb147dd44
John Rothe <forget@tempusmud.com>
2002-04-20 18:26:02 +0000

    Added a PlayerTable structure to replace the current linked list of id/name pairs.  It has better worst case but may have a signifigant overhead.
    Not used, just stored here for now.

---
+++ src/include/player_table.h
@@ -0,0 +1,173 @@
+#ifndef __PLAYER_TABLE_H__
+#define __PLAYER_TABLE_H__
+
+#include <vector>
+#include <pair.h>
+#include <algorithm>
+using namespace std;
+
+/** A class representing the "name" half of a player table entry. **/
+class NameEntry;
+/** A class representing the "player id" half of a player table entry. **/
+class IDEntry;
+
+
+
+/**
+ * A Class representing the "name" half of a player table.
+ * Contains NameEntry's.
+**/
+class NameTable : public vector<NameEntry> {
+    public:
+        NameTable() : vector<NameEntry>() { }
+        void sort();
+    private:
+};
+
+/**
+ * A Class representing the "ID" half of a player table.
+ * Contains NameEntry's.
+**/
+class IDTable : public vector<IDEntry> {
+    public:
+        IDTable() : vector<IDEntry>() { }
+        void sort();
+    private:
+};
+
+
+
+/**
+ * An ID->Name mapping for Players.
+ * Represented as a pair of sorted vectors of name/id pairs. One sorted by
+ * name and the other by id.  Lookups are done via a binary search of the
+ * appropriate vector.
+**/
+class PlayerTable 
+{
+    private:
+        IDTable idTable;
+        NameTable nameTable;
+    public:
+        /** Creates a blank PlayerTable  **/
+        PlayerTable();
+
+        /** Returns true if and only if the given id is present in the player table. **/
+        bool exists( long id );
+        /** Returns true if and only if the given name is present in the player table. **/
+        bool exists( const char* name );
+        
+        /** returns the char's name or NULL if not found. **/
+        const char *getName( long id );
+        /** returns chars id or 0 if not found **/
+        long getID( const char *name );
+
+        /** returns chars id or 0 if not found **/
+        long operator[](const char *name){ return getID(name); }
+        /** Returns the char's name or NULL if not found. **/
+        const char* operator[](long id) { return getName(id); }
+
+        /** Adds the given player info to this PlayerTable.  **/
+        bool add( long id, const char* name );
+        /** Removes the entry in this player table that corressponds to the given id. **/
+        bool remove( long id );
+        /** Removes the entry in this player table that corressponds to the given name. **/
+        bool remove( const char* name );
+        
+        /** Sorts the player table. **/
+        void sort();
+};
+
+
+
+
+/**
+ * A class representing the "name" half of a player table entry.
+ * It is nothing more than a char* / playerid pair
+ *
+ * Comparison operators have been overloaded for convinience with
+ * the lexographical value of the "char*" being compared.
+ *
+ *  Equivilance to both char* and long have been provided for
+ *  convinience.
+**/
+class NameEntry : public pair<char*, long> {
+    public:
+        NameEntry( long id, char* name );
+        NameEntry( const NameEntry &n );
+        ~NameEntry();
+        bool operator==(long id) const;
+        bool operator==( const char *name) const;
+        bool operator==(NameEntry &e) const;
+        bool operator!=(NameEntry &e) const;
+        bool operator<(NameEntry &e) const;
+        bool operator>(NameEntry &e) const;
+        bool operator<(long id) const;
+        bool operator>(long id) const;
+        bool operator<(const char* name) const;
+        bool operator>(const char* name) const;
+};
+
+inline bool operator==(long id, const NameEntry &e) { 
+    return e == id; 
+}
+inline bool operator<(long id, const NameEntry &e) { 
+    return e > id; 
+}
+inline bool operator>(long id, const NameEntry &e) { 
+    return e < id; 
+}
+inline bool operator==(const char* name, const NameEntry &e) { 
+    return e == name;
+}
+inline bool operator<(const char* name, const NameEntry &e) { 
+    return e > name;
+}
+inline bool operator>(const char* name, const NameEntry &e) { 
+    return e < name;
+}
+
+
+
+/**
+ * A class representing the "player id" half of a player table entry.
+ * It is nothing more than a char* / playerid pair
+ *
+ * Comparison operators have been overloaded for convinience with
+ * the value if the player id being compared.
+ *
+ * Equivilance to both char* and long have been provided for
+ * convinience.
+**/
+class IDEntry : public pair<long, char*> {
+    public:
+        IDEntry( long id, char* name);
+        IDEntry( const IDEntry &e );
+        // Destructory does NOT free name.
+        ~IDEntry();
+        
+        
+        bool operator==(IDEntry &e) const;
+        bool operator!=(IDEntry &e) const;
+        bool operator<(IDEntry &e) const;
+        bool operator>(IDEntry &e) const;
+        bool operator==( const char *name) const;
+        bool operator==(long id) const;
+        bool operator<(long id) const;
+        bool operator>(long id) const;
+};
+
+inline bool operator==(long id, const IDEntry &e) { 
+    return e == id; 
+}
+inline bool operator<(long id, const IDEntry &e) { 
+    return e > id; 
+}
+inline bool operator>(long id, const IDEntry &e) { 
+    return e < id; 
+}
+inline bool operator==(const char* name, const IDEntry &e) { 
+    return e == name;
+}
+
+#endif // __PLAYER_TABLE_H__
---
+++ src/structs/player_table.cc
@@ -0,0 +1,234 @@
+#include <string.h>
+#include "player_table.h"
+
+/**                 PLAYER TABLE                **/
+
+/**
+ *  Creates a blank PlayerTable
+**/
+PlayerTable::PlayerTable() 
+: idTable(), nameTable() { }
+
+/**
+ * Returns true if and only if the given id is present in the player table.
+**/
+bool PlayerTable::exists( long id ) 
+{
+    return binary_search( idTable.begin(), idTable.end(), id );
+}
+
+/**
+ * Returns true if and only if the given name is present in the player table.
+**/
+bool PlayerTable::exists( const char* name ) 
+{
+    return binary_search( nameTable.begin(), nameTable.end(), name );
+}
+
+/**
+ * returns the char's name or NULL if not found.
+**/
+const char *PlayerTable::getName( long id ) 
+{
+    IDTable::iterator it;
+    it = lower_bound( idTable.begin(), idTable.end(), id );
+    if( it != idTable.end() && (*it) == id ) {
+        return (*it).second;
+    }
+    return NULL;
+}
+
+/**
+ * returns chars id or 0 if not found
+ *
+**/
+long PlayerTable::getID( const char *name ) 
+{
+    NameTable::iterator it;
+    it = lower_bound( nameTable.begin(), nameTable.end(), name );
+    if( it != nameTable.end() && (*it) == name ) {
+        return (*it).second;
+    }
+    return 0;
+}
+
+/**
+ * Adds the given player info to this PlayerTable.
+ *
+ * NOTE: name will be duplicated and stored without alteration.
+**/
+bool PlayerTable::add( long id, const char* name ) 
+{
+    if( exists(id) )
+        return false;
+
+    idTable.push_back(IDEntry(id,strdup(name)));
+    nameTable.push_back(NameEntry(id,strdup(name)));
+    sort();
+    return true;
+}
+
+/**
+ * Removes the entry in this player table that corressponds to the
+ * given id.
+**/
+bool PlayerTable::remove( long id ) 
+{
+    NameTable::iterator nit;
+    IDTable::iterator iit;
+
+    iit = lower_bound( idTable.begin(), idTable.end(), id );
+    nit = lower_bound( nameTable.begin(), nameTable.end(), (*iit).second );
+
+    if( nit != nameTable.end() && (*nit) == id 
+    && iit != idTable.end() && (*iit) == id ) 
+    {
+        nameTable.erase(nit);
+        idTable.erase(iit);
+        return true;
+    }
+    return false; 
+}
+
+/**
+ * Removes the entry in this player table that corressponds to the
+ * given name.
+**/
+bool PlayerTable::remove( const char* name ) 
+{
+    NameTable::iterator nit;
+    IDTable::iterator iit;
+
+    nit = lower_bound( nameTable.begin(), nameTable.end(), name );
+    iit = lower_bound( idTable.begin(), idTable.end(), (*nit).second );
+
+    if( nit != nameTable.end() && (*nit) == name 
+    && iit != idTable.end() && (*iit) == name ) 
+    {
+        nameTable.erase(nit);
+        idTable.erase(iit);
+        return true;
+    }
+    return false; 
+}
+
+/**
+ * Sorts the player table.
+ * Calls sort on the two contained table halfs ( id and name ).
+**/
+void PlayerTable::sort() 
+{
+    idTable.sort();
+    nameTable.sort();
+}
+
+
+
+
+
+// Sorts the name table
+void NameTable::sort() 
+{
+    std::sort( begin(), end() );
+}
+// Sorts the id table
+void IDTable::sort() 
+{
+    std::sort( begin(), end() );
+}
+
+
+/***               NAME ENTRY               ***/
+
+// Constructor
+
+NameEntry::NameEntry( long id, char* name ) 
+: pair<char*, long>( name, id ) 
+{
+    //
+}
+
+// Copy Constructor
+NameEntry::NameEntry( const NameEntry &n ) {
+    this->first = strdup(n.first);
+    this->second = n.second;
+}
+// Destructor
+NameEntry::~NameEntry() {
+    free(first);
+}
+
+bool NameEntry::operator==(long id) const { 
+    return second == id; 
+}
+bool NameEntry::operator==(const char *name) const { 
+    return strcasecmp(first,name) == 0; 
+}
+bool NameEntry::operator==(NameEntry &e) const { 
+    return first == e.first; 
+}
+bool NameEntry::operator!=(NameEntry &e) const { 
+    return first != e.first; 
+}
+bool NameEntry::operator<(NameEntry &e) const { 
+    return ( strcasecmp(first,e.first) < 0);
+}
+bool NameEntry::operator>(NameEntry &e) const { 
+    return strcasecmp(first,e.first) > 0;
+}
+bool NameEntry::operator<(long id) const { 
+    return second < id; 
+}
+bool NameEntry::operator>(long id) const { 
+    return second > id; 
+}
+bool NameEntry::operator<(const char *name) const { 
+    return strcasecmp(first,name) < 0; 
+}
+bool NameEntry::operator>(const char *name) const { 
+    return strcasecmp(first,name) > 0; 
+}
+
+
+
+
+/***               ID ENTRY               ***/
+
+//Constructor
+IDEntry::IDEntry( long id, char* name) 
+: pair< long, char*>( id, name )
+{ }
+// Copy constructor
+IDEntry::IDEntry( const IDEntry &e ) {
+    this->second = strdup(e.second);
+    this->first = e.first;
+}
+// Destructor
+IDEntry::~IDEntry() {
+    free(second);
+}
+
+bool IDEntry::operator==(long id) const { 
+    return first == id; 
+}
+bool IDEntry::operator==(const char *name) const { 
+    return strcasecmp(second,name) == 0; 
+}
+bool IDEntry::operator==(IDEntry &e) const { 
+    return first == e.first; 
+}
+bool IDEntry::operator!=(IDEntry &e) const { 
+    return first != e.first; 
+}
+bool IDEntry::operator<(IDEntry &e) const { 
+    return first < e.first; 
+}
+bool IDEntry::operator>(IDEntry &e) const { 
+    return first > e.first; 
+}
+bool IDEntry::operator<(long id) const { 
+    return first < id; 
+}
+bool IDEntry::operator>(long id) const { 
+    return first > id; 
+}
