22cd7eef59f2d7837c2cc29d62550c8a40dcb32e
Daniel Lowe <azimuth@tempusmud.com>
2002-10-22 16:50:04 +0000

    Missed a header file. :P

--- src/include/login.h
+++ src/include/login.h
@@ -12,11 +12,10 @@ void show_menu(struct descriptor_data *d);
 void show_pc_race_menu(struct descriptor_data *d);
 void show_race_restrict(struct descriptor_data *d, int timeframe = 0);
 void show_char_class_menu(struct descriptor_data *d, bool remort);
+void show_pc_race_help(descriptor_data *d, char *arg);
+void show_pc_class_help(descriptor_data *d, char *arg);
 
-int parse_past_home(struct descriptor_data *d, char *arg);
 int parse_pc_race(struct descriptor_data *d, char *arg);
-int parse_future_home(struct descriptor_data *d, char *arg);
-
 
 #define MODE_RENT_MENU 0
 #define MODE_SHOW_MENU 1
