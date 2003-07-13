9af31de1eacacb404eb145f9e29925aec4327b45
Daniel Lowe <azimuth@tempusmud.com>
2003-07-13 04:16:49 +0000

    Creatures without shield mastery no longer deflect attacks with their shield.

--- src/combat/fight.cc
+++ src/combat/fight.cc
@@ -936,6 +936,7 @@ damage(struct Creature *ch, struct Creature *victim, int dam,
 
 	if (IS_WEAPON(attacktype)
 			&& GET_EQ(victim, WEAR_SHIELD)
+			&& CHECK_SKILL(victim, SKILL_SHIELD_MASTERY) > 20
 			&& victim->getLevelBonus(SKILL_SHIELD_MASTERY) > number(0, 600)) {
 		act("$N deflects your attack with $S shield!", true,
 			ch, GET_EQ(victim, WEAR_SHIELD), victim, TO_CHAR);
