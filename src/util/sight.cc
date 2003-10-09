b23b298bce45c46a948cb37ed1593f70dbdcb044
Daniel Lowe <azimuth@tempusmud.com>
2003-10-09 21:46:52 +0000

    NULL room check in check_vis_room() now generates more return addresses.

--- src/util/sight.cc
+++ src/util/sight.cc
@@ -84,10 +84,12 @@ check_sight_room(Creature *self, room_data *room)
 {
 	if (!room) {
 		slog("SYSERR: check_sight_room() called with NULL room");
-		slog( tmp_sprintf("        stack trace: 0x%lx 0x%lx 0x%lx\n",
-					(long)__builtin_return_address(0),
-					(long)__builtin_return_address(1),
-					(long)__builtin_return_address(2)));
+		slog("  stack trace: 0x%lx 0x%lx 0x%lx 0x%lx 0x%lx",
+			(long)__builtin_return_address(0),
+			(long)__builtin_return_address(1),
+			(long)__builtin_return_address(2),
+			(long)__builtin_return_address(3),
+			(long)__builtin_return_address(4));
 		return false;
 	}
 
