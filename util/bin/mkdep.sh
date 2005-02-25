c9a10556d6f9ac8525acfab81d2e7ae9831a5355
Daniel Lowe <azimuth@tempusmud.com>
2005-02-25 21:28:22 +0000

    Dependancy generation no longer sucks ass.

--- util/bin/mkdep.sh
+++ util/bin/mkdep.sh
@@ -2,40 +2,18 @@
 
 objlist=""
 
-for file in $*
-do
-    objname="\$(OBJ)/"`basename $file | sed -e 's/\.cc$/\.o/'`
-    objlist="$objlist $objname"
+CXX=g++
+INCLUDE="../include"
+CCFLAGS="-g -Wall -Wcast-qual -I/usr/include/libxml2 -I/usr/local/pgsql/include -I/usr/include/pgsql"
+CXXFLAGS="$CCFLAGS -I$INCLUDE"
 
-    echo "$objname : $file \\"
-
-    found=0
-
-    for incl in `grep "#include" $file | awk -F\" '{print $2}'`
-    do
-	if [ $found = 0 ]
-	then
-	    printf "\t"
-	    found=1
-	fi
-
-	if echo $incl | grep "Specs" > /dev/null 2>&1
-	then
-	    echo -n "$incl "
-	else
-	    echo -n "\$(INCLUDE)/$incl "
-	fi
-    done
-
-    printf "\n"
-    printf "\t\$(CXX) -c \$(CXXFLAGS) %-20s -o $objname" $file
-    printf "\n"
-
-done
+for file in $*; do
+objname="\$(OBJ)/"`basename $file | sed -e 's/\.cc$/\.o/'`
+objlist="$objlist $objname"
+printf "\$(OBJ)/";
+if $CXX ${CXXFLAGS} -MM $file; then
+	printf "\t\$(CXX) \$(CXXFLAGS) -o \$@ -c \$<\n"
+fi
+done;
 
 echo OBJFILES=$objlist
-exit
-
-
-
-
