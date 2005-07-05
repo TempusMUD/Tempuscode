179dbb132e986dc4580324ffcbdfbab18b7f52f5
Lenon Kitchens <nothing@tempusmud.com>
2005-07-05 17:44:44 +0000

    Houses now actually put objects into the object list upon load

--- src/house/house.cc
+++ src/house/house.cc
@@ -683,9 +683,7 @@ House::loadRoom( xmlNodePtr roomNode )
 	for ( xmlNodePtr node = roomNode->xmlChildrenNode; node; node = node->next ) 
 	{
         if ( xmlMatches(node->name, "object") ) {
-			obj_data *obj;
-			CREATE(obj, obj_data, 1);
-			obj->clear();
+			obj_data *obj = create_obj();
 			if(! obj->loadFromXML(NULL, NULL, room, node) ) {
 				extract_obj(obj);
 			}
