c33ec0acea169f531296418cb8b7830636325aec
Lenon Kitchens <nothing@tempusmud.com>
2005-07-09 19:00:54 +0000

    The message about no longer being innocent now reads first generation instead of fifth

--- src/net/nanny.cc
+++ src/net/nanny.cc
@@ -1493,7 +1493,7 @@ char_to_game(descriptor_data *d)
     // if their rep is 0 and they are >= gen 5 they gain 5 rep
 	if (GET_REMORT_GEN(d->creature) >= 1 && d->creature->get_reputation() == 0) {
         d->creature->gain_reputation(5);
-        notes = tmp_strcat(notes, "You are no longer innocent because you have reached your fifth generation.\r\n");
+        notes = tmp_strcat(notes, "You are no longer innocent because you have reached your first generation.\r\n");
     }
     
 	d->creature->player.time.logon = now;
