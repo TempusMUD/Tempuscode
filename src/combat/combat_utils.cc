3e68955635fff4a9f146104b7439eadc9e6a936c
Logic <logic@tempusmud.com>
2005-08-08 02:38:32 +0000

    The non-corporeal should no longer leave body parts lying around when killed.

--- src/combat/combat_utils.cc
+++ src/combat/combat_utils.cc
@@ -908,7 +908,7 @@ make_corpse(struct Creature *ch, struct Creature *killer, int attacktype)
 		corpse->line_desc = str_dup(buf2);
 		strcpy(adj, "legless");
 
-		if (IS_RACE(ch, RACE_BEHOLDER))
+		if (IS_RACE(ch, RACE_BEHOLDER) || NON_CORPOREAL_UNDEAD(ch))
 			break;
 
 		leg = create_obj();
@@ -1268,7 +1268,7 @@ make_corpse(struct Creature *ch, struct Creature *killer, int attacktype)
 
 	case SPELL_TAINT:
 	case TYPE_TAINT_BURN:
-		if (IS_RACE(ch, RACE_BEHOLDER)) {
+		if (IS_RACE(ch, RACE_BEHOLDER) || NON_CORPOREAL_UNDEAD(ch)) {
 			// attack that rips the victim's head off
 			sprintf(buf2, "The smoking %s of %s %s lying here.",
 				typebuf, GET_NAME(ch), isare);
@@ -1336,7 +1336,7 @@ make_corpse(struct Creature *ch, struct Creature *killer, int attacktype)
 		corpse->line_desc = str_dup(buf2);
 		sprintf(adj, "headless");
 
-		if (IS_RACE(ch, RACE_BEHOLDER))
+		if (IS_RACE(ch, RACE_BEHOLDER) || NON_CORPOREAL_UNDEAD(ch))
 			break;
 
 		head = create_obj();
