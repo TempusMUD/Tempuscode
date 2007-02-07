1b2d66e5e9ef0ef3b1104b0fa14913787cb9941a
Daniel Lowe <azimuth@tempusmud.com>
2007-02-07 14:19:55 +0000

    Crash report script added to repo

---
+++ bin/crash_report.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+MAIL_RECIPIENTS=crashreport@tempusmud.com
+
+function print_report() {
+
+    LINECOUNT=`wc -l $0 | awk '{print $1}'`
+    CUTLINE=`grep --line-number "^\# GDB COMMAND LIST CUT HERE" $0 | awk -F: '{print $1}'`
+    TAILCOUNT=`echo $((LINECOUNT-CUTLINE))`
+
+
+    echo " >>>>"
+    echo " >>>> MUD CRASH REPORT <<<<"
+    echo " >>>> `date`"
+    echo " >>>>"
+    echo
+    echo " >>>>"
+    echo " >>>> TAIL of last syslog (log/syslog.0)"
+    echo " >>>>"
+    echo
+
+    tail log/syslog.0
+
+    echo
+    echo " >>>>"
+    echo " >>>> GDB OUTPUT"
+    echo " >>>>"
+    echo
+
+    tail -${TAILCOUNT} $0 > /tmp/tempus.crash.cmds
+	chmod g+r lib/core
+	gdb bin/circle lib/core </tmp/tempus.crash.cmds
+}
+
+
+print_report $0 | mail -s "TempusMUD CRASH REPORT" ${MAIL_RECIPIENTS}
+
+
+exit
+
+# gbd coms
+#
+# GDB COMMAND LIST CUT HERE
+set prompt \n
+set print null-stop on
+set print pretty on
+printf " >>>>\n >>>> STACK TRACE\n >>>>\n"
+bt
+printf " >>>>\n >>>> LAST CMD ISSUED\n >>>>\n"
+print last_cmd
+quit
