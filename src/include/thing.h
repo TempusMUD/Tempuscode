61db0c41b881e0a04069d4a301efc0ce45cc6896
Daniel Lowe <azimuth@tempusmud.com>
2007-12-25 02:12:13 +0000

    Attempt to fix compiler warnings about const qualifiers

--- src/include/thing.h
+++ src/include/thing.h
@@ -31,22 +31,22 @@ public:
     inline Creature *to_c(void) const {
         if (_kind != CREATURE)
             raise(SIGSEGV);
-        return (Creature *)this;
+        return reinterpret_cast<Creature *>(const_cast<thing *>(this));
     }
     inline obj_data *to_o(void) const {
         if (_kind != OBJECT)
             raise(SIGSEGV);
-        return (obj_data *)this;
+        return reinterpret_cast<obj_data *>(const_cast<thing *>(this));
     }
     inline room_data *to_r(void) const {
         if (_kind != ROOM)
             raise(SIGSEGV);
-        return (room_data *)this;
+        return reinterpret_cast<room_data *>(const_cast<thing *>(this));
     }
     inline zone_data *to_z(void) const {
         if (_kind != ZONE)
             raise(SIGSEGV);
-        return (zone_data *)this;
+        return reinterpret_cast<zone_data *>(const_cast<thing *>(this));
     }
 private:
     thing_kind _kind;
