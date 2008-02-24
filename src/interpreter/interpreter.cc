3013e583847b9899a7ac2010bd6e1350abefc6cd
Daniel Lowe <azimuth@tempusmud.com>
2008-02-24 18:03:07 -0500

    Changed "transfer" command proc to do_not_here.

--- src/interpreter/interpreter.cc
+++ src/interpreter/interpreter.cc
@@ -1384,7 +1384,7 @@ struct command_info cmd_info[] = {
 	{"train", POS_STANDING, do_practice, 1, 0, 0, 0},
 	{"trample", POS_STANDING, do_not_here, 0, 0, 0, 0},
 	{"transport", POS_SLEEPING, do_transport, 0, 0, 0, 0},
-	{"transfer", POS_SLEEPING, do_action, 0, 0, 0, 0},
+	{"transfer", POS_SLEEPING, do_not_here, 0, 0, 0, 0},
 	{"translocate", POS_RESTING, do_translocate, 20, ZEN_TRANSLOCATION, 0, 0},
 	{"transmit", POS_RESTING, do_transmit, 0, 0, 0, 0},
     {"tremble", POS_RESTING, do_action, 0, 0, 0, 0},
