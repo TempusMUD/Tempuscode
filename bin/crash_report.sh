2d75c347d361af3ae91daaecf72f5cde16073838
Daniel Lowe <azimuth@tempusmud.com>
2008-05-03 17:56:57 -0400

    Crash report now generates proper reports

--- bin/crash_report.sh
+++ bin/crash_report.sh
@@ -1,39 +1,31 @@
 #!/bin/bash
 
-MAIL_RECIPIENTS=crashreport@tempusmud.com
-
-function print_report() {
-
-    LINECOUNT=`wc -l $0 | awk '{print $1}'`
-    CUTLINE=`grep --line-number "^\# GDB COMMAND LIST CUT HERE" $0 | awk -F: '{print $1}'`
-    TAILCOUNT=`echo $((LINECOUNT-CUTLINE))`
+LINECOUNT=`wc -l $0 | awk '{print $1}'`
+CUTLINE=`grep --line-number "^\# GDB COMMAND LIST CUT HERE" $0 | awk -F: '{print $1}'`
+TAILCOUNT=`echo $((LINECOUNT-CUTLINE))`
 
 
-    echo " >>>>"
-    echo " >>>> MUD CRASH REPORT <<<<"
-    echo " >>>> `date`"
-    echo " >>>>"
-    echo
-    echo " >>>>"
-    echo " >>>> TAIL of last syslog (log/syslog.0)"
-    echo " >>>>"
-    echo
+echo " >>>>"
+echo " >>>> MUD CRASH REPORT <<<<"
+echo " >>>> `date`"
+echo " >>>>"
+echo
+echo " >>>>"
+echo " >>>> TAIL of last syslog (log/syslog.0)"
+echo " >>>>"
+echo
 
-    tail $1
+tail $2
 
-    echo
-    echo " >>>>"
-    echo " >>>> GDB OUTPUT"
-    echo " >>>>"
-    echo
+echo
+echo " >>>>"
+echo " >>>> GDB OUTPUT"
+echo " >>>>"
+echo
 
-    tail -${TAILCOUNT} $0 > /tmp/tempus.crash.cmds
-	chmod g+r lib/core
-	gdb bin/circle lib/core </tmp/tempus.crash.cmds
-}
-
-
-print_report $0 $1
+tail -${TAILCOUNT} $0 > /tmp/tempus.crash.cmds
+gdb bin/circle $1 </tmp/tempus.crash.cmds
+chmod g+r $1
 
 exit
 
