611bea1c94a6db543e0bd8acc7370c9206af2040
Daniel Lowe <azimuth@tempusmud.com>
2010-08-26 09:11:32 -0400

    Prog editor converted to C

--- src/editor/progeditor.c
+++ src/editor/progeditor.c
@@ -23,72 +23,72 @@
 #include "accstr.h"
 #include "help.h"
 #include "comm.h"
-#include "player_table.h"
+
+struct progeditor_data {
+    void *owner;
+    enum prog_evt_type owner_type;
+};
 
 void
-start_editing_prog(descriptor_data *d, void *owner, prog_evt_type owner_type)
+progeditor_finalize(struct editor *editor, const char *text)
 {
-	if (d->text_editor) {
-		errlog("Text editor object not null in start_editing_mail");
-		REMOVE_BIT(PLR_FLAGS(d->creature),
-			PLR_WRITING | PLR_OLC | PLR_MAILING);
-		return;
-	}
-
-    SET_BIT(PLR_FLAGS(d->creature), PLR_WRITING | PLR_OLC);
-
-	d->text_editor = new CProgEditor(d, owner, owner_type);
-}
-
-CProgEditor_CProgEditor(descriptor_data *desc,
-                         void *o,
-                         prog_evt_type t)
-    : CEditor(desc, MAX_STRING_LENGTH)
-{
-    owner = o;
-    owner_type = t;
-    ImportText(prog_get_text(owner, owner_type));
-
-    SendStartupMessage();
-    DisplayBuffer();
-}
-
-bool
-CProgEditor_PerformCommand(char cmd, char *args)
-{
-    switch (cmd) {
-    case 'u':
-        UndoChanges();
-        break;
-    default:
-        return CEditor_PerformCommand(cmd, args);
-    }
-
-    return true;
-}
-
-void
-CProgEditor_Finalize(const char *text)
-{
+    struct progeditor_data *prog_data = (struct progeditor_data *)editor->mode_data;
     char *new_prog = (*text) ? strdup(text):NULL;
 
-    switch (owner_type) {
+    switch (prog_data->owner_type) {
     case PROG_TYPE_MOBILE:
-        if (GET_MOB_PROG((struct creature *)owner))
-            free(GET_MOB_PROG((struct creature *)owner));
-        ((struct creature *)owner)->mob_specials.shared->prog = new_prog;
+        if (GET_MOB_PROG((struct creature *)prog_data->owner))
+            free(GET_MOB_PROG((struct creature *)prog_data->owner));
+        ((struct creature *)prog_data->owner)->mob_specials.shared->prog = new_prog;
         break;
     case PROG_TYPE_ROOM:
-        if (GET_ROOM_PROG((struct room_data *)owner))
-            free(((struct room_data *)owner)->prog);
-        ((struct room_data *)owner)->prog = new_prog;
+        if (GET_ROOM_PROG((struct room_data *)prog_data->owner))
+            free(((struct room_data *)prog_data->owner)->prog);
+        ((struct room_data *)prog_data->owner)->prog = new_prog;
         break;
     default:
         errlog("Can't happen");
     }
 
-    prog_compile(desc->creature, owner, owner_type);
+    prog_compile(editor->desc->creature, prog_data->owner, prog_data->owner_type);
 
-    if (IS_PLAYING(desc))
-        act("$n nods with satisfaction as $e saves $s work.", true, desc->creature, 0, 0, TO_NOTVICT);
+    if (IS_PLAYING(editor->desc))
+        act("$n nods with satisfaction as $e saves $s work.",
+            true, editor->desc->creature, 0, 0, TO_NOTVICT);
+    free(prog_data);
 }
+
+void
+progeditor_cancel(struct editor *editor)
+{
+    free(editor->mode_data);
+}
+
+void
+start_editing_prog(struct descriptor_data *d,
+                   void *owner,
+                   enum prog_evt_type owner_type)
+{
+	struct progeditor_data *prog_data;
+	if (d->text_editor) {
+		errlog("Text editor object not null in start_editing_prog");
+		REMOVE_BIT(PLR_FLAGS(d->creature),
+                   PLR_WRITING | PLR_OLC | PLR_MAILING);
+		return;
+	}
+
+    SET_BIT(PLR_FLAGS(d->creature), PLR_WRITING | PLR_OLC);
+
+	d->text_editor = make_editor(d, MAX_STRING_LENGTH);
+    CREATE(prog_data, struct progeditor_data, 1);
+    d->text_editor->finalize = progeditor_finalize;
+    d->text_editor->cancel = progeditor_cancel;
+    d->text_editor->mode_data = prog_data;
+
+    prog_data->owner = owner;
+    prog_data->owner_type = owner_type;
+
+    emit_editor_startup(d->text_editor);
+    display_buffer(d->text_editor);
+}
+
