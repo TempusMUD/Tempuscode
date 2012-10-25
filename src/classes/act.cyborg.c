74c792ea311ee1a24c8c6d185ac5e9fef4f60595
Mike Clemson <mikeclemson@gmail.com>
2012-10-25 14:38:01 -0400

    Cyborepair now checks more positions for repair tools

--- src/classes/act.cyborg.c
+++ src/classes/act.cyborg.c
@@ -1872,11 +1872,10 @@ ACMD(do_repair)
 
     if (vict) {
         if (!IS_NPC(ch) && (
-                (!(tool = GET_IMPLANT(ch, WEAR_HOLD)) &&
-                    !(tool = GET_EQ(ch, WEAR_HOLD))) ||
+                (!(tool = GET_IMPLANT(ch, WEAR_HOLD)) && !(tool = GET_IMPLANT(ch, WEAR_HANDS)) && !(tool = GET_IMPLANT(ch, WEAR_WRIST_L)) && !(tool = GET_IMPLANT(ch, WEAR_WRIST_R)) && !(tool = GET_IMPLANT(ch, WEAR_FINGER_L)) && !(tool = GET_IMPLANT(ch, WEAR_FINGER_R)) && !(tool = GET_EQ(ch, WEAR_HOLD))) ||
                 !IS_TOOL(tool) || TOOL_SKILL(tool) != SKILL_CYBOREPAIR)) {
             send_to_char(ch,
-                "You must be holding a cyber repair tool to do this.\r\n");
+                "You must operate a cyborepair tool to do this.\r\n");
             return;
         }
 
