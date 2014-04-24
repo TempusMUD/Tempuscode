79a89c08d8a5b6424b7c97d425312c873c35a1b4
Daniel Lowe <azimuth@tempusmud.com>
2014-04-24 19:49:33 -0400

    Fix race condition and error handling in file editor

--- src/editor/fileeditor.c
+++ src/editor/fileeditor.c
@@ -11,9 +11,9 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <ctype.h>
-#include <sys/stat.h>
 #include <unistd.h>
 #include <glib.h>
+#include <errno.h>
 
 #include "utils.h"
 #include "constants.h"
@@ -55,7 +55,6 @@ start_editing_file(struct descriptor_data *d, const char *fname)
 {
     FILE *inf = NULL;
     char *target;
-    struct stat sbuf;
 
     if (!d) {
         errlog("NULL desriptor passed into start_editing_file()!!");
@@ -63,8 +62,7 @@ start_editing_file(struct descriptor_data *d, const char *fname)
     }
     if (!fname) {
         errlog("NULL file name pointer passed into start_editing_file()!!");
-        send_to_char(d->creature,
-            "This command seems to be broken. Bug this.\r\n");
+        d_printf(d, "This command seems to be broken. Bug this.\r\n");
         return;
     }
     if (d->text_editor) {
@@ -72,48 +70,47 @@ start_editing_file(struct descriptor_data *d, const char *fname)
         return;
     }
 
-    if (access(fname, W_OK | R_OK)) {
-        errlog("Unable to edit requested file (%s) in start_editing_file()!",
-            fname);
-        send_to_char(d->creature, "I'm unable to edit %s.  Bug this.\r\n",
-            fname);
-        return;
-    }
-
-    stat(fname, &sbuf);
-    if (sbuf.st_size > MAX_EDIT_FILESIZE) {
-        errlog("Attempt to edit file too large for editor!");
-        send_to_char(d->creature, "%s is too large. Bug this.\r\n", fname);
-        return;
-    }
-
     inf = fopen(fname, "r");
-    if (inf) {
-        CREATE(target, char, (size_t)sbuf.st_size + 1);
-        if (!target) {
-            errlog("Couldn't allocate memory to edit %s", fname);
-            d_send(d, "Internal error #09384\r\n");
-            return;
-        }
+    if (inf == NULL) {
+        errlog("File editor couldn't open file %s: %s", fname, strerror(errno));
+        d_printf(d, "Couldn't open file.  Sorry.\r\n");
+        return;
+    }
+    
+    off_t len = lseek(fileno(inf), 0, SEEK_END);
 
-        if (fread(target, sizeof(char), (size_t)sbuf.st_size,
-                inf) != (size_t) sbuf.st_size) {
-            d_printf(d, "Internal error #42372\r\n");
-            free(target);
-            fclose(inf);
-            return;
-        }
+    rewind(inf);
 
-        d->text_editor = make_editor(d, MAX_EDIT_FILESIZE);
-        editor_import(d->text_editor, target);
+    if (len > MAX_EDIT_FILESIZE) {
+        errlog("Attempt to edit file too large for editor!");
+        d_printf(d, "%s is too large. Bug this.\r\n", fname);
+        return;
+    }
 
-        SET_BIT(PLR_FLAGS(d->creature), PLR_WRITING);
+    
+    CREATE(target, char, (size_t)len + 1);
+    if (!target) {
+        errlog("Couldn't allocate memory to edit %s", fname);
+        d_send(d, "Internal error #09384\r\n");
+        return;
+    }
 
-        d->text_editor->finalize = fileeditor_finalize;
-        d->text_editor->mode_data = strdup(fname);
+    if (fread(target, sizeof(char), (size_t)len, inf) != (size_t) len) {
+        d_printf(d, "Internal error #42372\r\n");
         free(target);
         fclose(inf);
-
-        emit_editor_startup(d->text_editor);
+        return;
     }
+
+    d->text_editor = make_editor(d, MAX_EDIT_FILESIZE);
+    editor_import(d->text_editor, target);
+
+    SET_BIT(PLR_FLAGS(d->creature), PLR_WRITING);
+
+    d->text_editor->finalize = fileeditor_finalize;
+    d->text_editor->mode_data = strdup(fname);
+    free(target);
+    fclose(inf);
+
+    emit_editor_startup(d->text_editor);
 }
