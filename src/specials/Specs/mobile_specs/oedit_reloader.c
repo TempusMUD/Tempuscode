5bf57de9b05d00d18d989dcb6ce43b6075ea7bf1
Mike Clemson <mikeclemson@gmail.com>
2014-05-07 13:33:32 -0400

    Oedit reloader now loads only if necessary

--- src/specials/Specs/mobile_specs/oedit_reloader.c
+++ src/specials/Specs/mobile_specs/oedit_reloader.c
@@ -17,6 +17,80 @@ get_top_container(struct obj_data *obj)
 }
 
 int
+retrieve_oedits(struct creature *ch, struct obj_data *inc_obj)
+{
+    int count = 0;
+
+    if (IS_NPC(ch))
+        return 0;
+
+    for (struct obj_data * obj = object_list; obj; obj = obj->next) {
+        if (inc_obj->shared->vnum == obj->shared->vnum && obj->shared->owner_id == GET_IDNUM(ch)) {
+            if (obj->worn_by == ch || obj->carried_by == ch) {
+                send_to_char(ch, "%s glows bright orange.\r\n", obj->name);
+                count++;
+                continue;
+            }
+            if (obj->in_obj) {
+                struct obj_data *tmp = get_top_container(obj);
+                if (tmp->carried_by == ch || tmp->worn_by == ch) {
+                    send_to_char(ch, "%s glows a faint orange, revealing owned items within.\r\n", tmp->name);
+                    count++;
+                    continue;
+                }
+            }
+
+            struct room_data *prev_obj_room = where_obj(obj);
+
+            if (obj->worn_by && obj == GET_EQ(obj->worn_by, obj->worn_on)) {
+                act("$p disappears off of your body!",
+                    false, obj->worn_by, obj, NULL, TO_CHAR);
+                unequip_char(obj->worn_by, obj->worn_on, EQUIP_WORN);
+                count++;
+            } else if (obj->worn_by
+                && obj == GET_IMPLANT(obj->worn_by, obj->worn_on)) {
+                act("$p disappears out of your body!", false, obj->worn_by,
+                    obj, NULL, TO_CHAR);
+                unequip_char(obj->worn_by, obj->worn_on, EQUIP_IMPLANT);
+                count++;
+            } else if (obj->worn_by
+                && obj == GET_TATTOO(obj->worn_by, obj->worn_on)) {
+                act("$p fades off of your body!", false, obj->worn_by, obj, NULL,
+                    TO_CHAR);
+                unequip_char(obj->worn_by, obj->worn_on, EQUIP_TATTOO);
+                count++;
+            } else if (obj->carried_by) {
+                act("$p disappears out of your hands!", false,
+                    obj->carried_by, obj, NULL, TO_CHAR);
+                obj_from_char(obj);
+                count++;
+            } else if (obj->in_room) {
+                if (obj->in_room->people) {
+                    act("$p fades out of existence!",
+                        false, NULL, obj, NULL, TO_ROOM);
+                }
+                obj_from_room(obj);
+                count++;
+            } else if (obj->in_obj != NULL) {
+                obj_from_obj(obj);
+                count++;
+            }
+
+            if (prev_obj_room && ROOM_FLAGGED(prev_obj_room, ROOM_HOUSE)) {
+                struct house *h = find_house_by_room(prev_obj_room->number);
+                if (h)
+                    save_house(h);
+            }
+
+            obj_to_char(obj, ch);
+            act("$p appears in your hands!", false, ch, obj, NULL, TO_CHAR);
+        }
+    }
+
+    return count;
+}
+
+int
 load_oedits(struct creature *ch)
 {
     GHashTableIter iter;
@@ -30,79 +104,16 @@ load_oedits(struct creature *ch)
     while (g_hash_table_iter_next(&iter, &key, &val)) {
         struct obj_data *obj = val;
         if (obj->shared->owner_id == GET_IDNUM(ch)) {
-            ++count;
-            struct obj_data *o = read_object(obj->shared->vnum);
-            obj_to_char(o, ch);
-            act("$p appears in your hands!", false, ch, o, NULL, TO_CHAR);
+            int num_world = retrieve_oedits(ch, obj);
+            if (!num_world) {
+                struct obj_data *o = read_object(obj->shared->vnum);
+                obj_to_char(o, ch);
+                act("$p appears in your hands!", false, ch, o, NULL, TO_CHAR);
+                count++;
+            }
         }
     }
 
-    return count;
-}
-
-int
-retrieve_oedits(struct creature *ch)
-{
-    int count = 0;
-
-    if (IS_NPC(ch))
-        return 0;
-
-    for (struct obj_data * obj = object_list; obj; obj = obj->next) {
-        if (obj->shared->owner_id == GET_IDNUM(ch)) {
-            // They already have it
-            if (obj->worn_by == ch || obj->carried_by == ch) {
-                continue;
-            }
-            if (obj->in_obj) {
-                struct obj_data *tmp = get_top_container(obj);
-                // They already have it
-                if (tmp->carried_by == ch || tmp->worn_by == ch) {
-                    continue;
-                }
-            }
-
-            struct room_data *prev_obj_room = where_obj(obj);
-
-            if (obj->worn_by && obj == GET_EQ(obj->worn_by, obj->worn_on)) {
-                act("$p disappears off of your body!",
-                    false, obj->worn_by, obj, NULL, TO_CHAR);
-                unequip_char(obj->worn_by, obj->worn_on, EQUIP_WORN);
-            } else if (obj->worn_by
-                && obj == GET_IMPLANT(obj->worn_by, obj->worn_on)) {
-                act("$p disappears out of your body!", false, obj->worn_by,
-                    obj, NULL, TO_CHAR);
-                unequip_char(obj->worn_by, obj->worn_on, EQUIP_IMPLANT);
-            } else if (obj->worn_by
-                && obj == GET_TATTOO(obj->worn_by, obj->worn_on)) {
-                act("$p fades off of your body!", false, obj->worn_by, obj, NULL,
-                    TO_CHAR);
-                unequip_char(obj->worn_by, obj->worn_on, EQUIP_TATTOO);
-            } else if (obj->carried_by) {
-                act("$p disappears out of your hands!", false,
-                    obj->carried_by, obj, NULL, TO_CHAR);
-                obj_from_char(obj);
-            } else if (obj->in_room) {
-                if (obj->in_room->people) {
-                    act("$p fades out of existence!",
-                        false, NULL, obj, NULL, TO_ROOM);
-                }
-                obj_from_room(obj);
-            } else if (obj->in_obj != NULL) {
-                obj_from_obj(obj);
-            }
-
-            if (prev_obj_room && ROOM_FLAGGED(prev_obj_room, ROOM_HOUSE)) {
-                struct house *h = find_house_by_room(prev_obj_room->number);
-                if (h)
-                    save_house(h);
-            }
-
-            ++count;
-            obj_to_char(obj, ch);
-            act("$p appears in your hands!", false, ch, obj, NULL, TO_CHAR);
-        }
-    }
     return count;
 }
 
@@ -123,16 +134,12 @@ SPECIAL(oedit_reloader)
     } else if (CMD_IS("retrieve")) {
         act("$n closes $s eyes in deep concentration.", true, self, NULL, NULL,
             TO_ROOM);
-        int retrieved = retrieve_oedits(ch);
-        int existing = load_oedits(ch);
+        int loaded = load_oedits(ch);
 
-        if (!existing) {
-            perform_say_to(self, ch, "You own nothing that can be retrieved.");
-        } else if (!retrieved) {
-            perform_say_to(self, ch,
-                "You already have all that could be retrieved.");
+        if (!loaded) {
+            perform_say_to(self, ch, "I couldn't retrieve any owned items for you.");
         } else {
-            perform_say_to(self, ch, "That should be everything.");
+            perform_say_to(self, ch, "That should be everything you own.");
         }
     } else {
         return false;
