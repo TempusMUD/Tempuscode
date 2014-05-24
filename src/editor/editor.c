28842e54e5682daf437de050de919f5c2fbd1f11
Daniel Lowe <azimuth@tempusmud.com>
2014-05-24 07:08:21 -0400

    Revert "Refactor basic editor commands to use tmpstr utilities"
    This reverts commit 8efaac865fd4b72e838044dc6105e6b18805cdb4.

--- src/editor/editor.c
+++ src/editor/editor.c
@@ -768,7 +768,7 @@ bool
 editor_do_command(struct editor * editor, char cmd, char *args)
 {
     int line, start_line, end_line, dest_line;
-    char *first_arg;
+    char command[MAX_INPUT_LENGTH];
 
     switch (tolower(cmd)) {
     case 'h':                  // Help
@@ -791,13 +791,13 @@ editor_do_command(struct editor * editor, char cmd, char *args)
         editor_emit(editor, "Cleared.\r\n");
         break;
     case 'l':                  // Replace Line
-         first_arg = tmp_getword(&args);
-        if (!isdigit(*first_arg)) {
+        args = one_argument(args, command);
+        if (!isdigit(*command)) {
             editor_emit(editor,
                 "Format for Replace Line is: &&l <line #> <text>\r\n");
             break;
         }
-        line = atoi(first_arg);
+        line = atoi(command);
         if (line < 1) {
             editor_emit(editor,
                 "Format for Replace Line is: &&l <line #> <text>\r\n");
@@ -806,13 +806,13 @@ editor_do_command(struct editor * editor, char cmd, char *args)
         editor_replace_line(editor, line, args);
         break;
     case 'i':                  // Insert Line
-         first_arg = tmp_getword(&args);
-        if (!isdigit(*first_arg)) {
+        args = one_argument(args, command);
+        if (!isdigit(*command)) {
             editor_emit(editor,
                 "Format for insert command is: &&i <line #> <text>\r\n");
             break;
         }
-        line = atoi(first_arg);
+        line = atoi(command);
         if (line < 1) {
             editor_emit(editor,
                 "Format for insert command is: &&i <line #><text>\r\n");
@@ -821,8 +821,8 @@ editor_do_command(struct editor * editor, char cmd, char *args)
         editor_insert(editor, line, args);
         break;
     case 'd':                  // Delete Line
-        first_arg = tmp_getword(&args);;
-        if (!parse_optional_range(first_arg, &start_line, &end_line)) {
+        args = one_argument(args, command);
+        if (!parse_optional_range(command, &start_line, &end_line)) {
             editor_emit(editor,
                 "Format for delete command is: &&d <line #>\r\n");
             break;
