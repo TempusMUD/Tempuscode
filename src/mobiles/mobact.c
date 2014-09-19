d6585b48bbfadb563141da456d19ae2d13b527ea
OriginTempus <cosine_omerta@yahoo.com>
2014-09-18 21:16:45 -0700

    Remove fire breathing wait state

--- src/mobiles/mobact.c
+++ src/mobiles/mobact.c
@@ -4223,7 +4223,8 @@ ACMD(do_breathe)
                 if (fire->duration <= 0)
                     affect_remove(ch, fire);
             }
-            WAIT_STATE(ch, 1 RL_SEC);
+            // the infamous fire breathing wait-state, currently not implemented
+            // WAIT_STATE(ch, 1 RL_SEC);
         } else if (frost != NULL) {
             send_to_char(ch, "ERROR: Frost breath not found.\r\n");
         } else {
