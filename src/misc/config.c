d2f2a7bde21615eb1539902df586d0703fc9b772
MorbidFinch <morbidfinch@gmail.com>
2015-10-03 04:30:09 -0400

    Restore admin email to greeting

--- src/misc/config.c
+++ src/misc/config.c
@@ -306,9 +306,9 @@ const char *GREETINGS[] = {
     ". . . -Md           :NMMMMMMMMMNNNmdo:`        -Ms\r\n"
     ". . .  -`         :hNMMMmyoosydmNNMMMNds:`     sM:    Powered by Linux .   .\r\n"
     ". . .:yNMNmy+:..:yNMMMd/`     ``..:sdNMMNms:.-sNh\r\n"
-    ". . +NhsyddNMMNNMMMmy-`             `-+ydNMMNNMm.      .   .   .   .   .   .\r\n"
-    ". . yy. .omh:+sdy+-`                    `.:yMMd.       \r\n"
-    ". . . . . /mm+``                          .yNs`   .    .   .   .   .   .   .\r\n"
+    ". . +NhsyddNMMNNMMMmy-`             `-+ydNMMNNMm.    Please email  .   .   .\r\n"
+    ". . yy. .omh:+sdy+-`                    `.:yMMd.    admin@tempusmud.com     \r\n"
+    ". . . . . /mm+``                          .yNs`    with any login issues.  .\r\n"
     ". . . . . .`oNm+.                       -sNh-\r\n"
     ". . . . . . .`+dNh+.                `-odmy-       visit www.tempusmud.com  .\r\n"
     ". . . . . . . . .ohmmhs+/--...-:/oydNds:`\r\n"
@@ -317,32 +317,6 @@ const char *GREETINGS[] = {
     ,
     "\n"
 };
-
-/* old version for reference when replacing email
-"\r\n"
-    ". . . . . . . . . . ./+syhdddddhyo/-`      .   .   .   .   .   .   .   .   .\r\n"
-    ". . . . . . . . -ohmdy+:-.````..:/ohmds:`\r\n"
-    ". . . . . . .`odms:`                 .+hms-      \\\\\\\\\\ TEMPUS /////    .   .\r\n"
-    ". . . . . ..sNh/`                       .omh-\r\n"
-    ". . . . . /md:          ,..._             `oNs`    the Anachronistic   .   .\r\n"
-    ". . . . `sNs`     .:/oyhdmNNmhy+:.  /ymmh.  -dd.    Multiple User Domain\r\n"
-    ". . . . sMo       +my/::::-:yMMMMMNmNMMMMs  .-dm.      .   .   .   .   .   .\r\n"
-    ". . . ./My         `      `/dMMMMMMMMMdy/` /mh:Nh     BASE: Circle 3,\r\n"
-    ". . .  mM.               :hMMMMMMMNNMMy+osyho- oM:    Jeremy Elson .   .   .\r\n"
-    ". . . -Md             `/hMMMMMMms:.-/shdy+.    -Ms    A derivative of\r\n"
-    ". . . :Mh            :mMMMMMMMs:...            .My    DikuMUD (GAMMA 0.0)  .\r\n"
-    ". . . -Md           :NMMMMMMMMMNNNmdo:`        -Ms\r\n"
-    ". . .  -`         :hNMMMmyoosydmNNMMMNds:`     sM:    Powered by Linux .   .\r\n"
-    ". . .:yNMNmy+:..:yNMMMd/`     ``..:sdNMMNms:.-sNh\r\n"
-    ". . +NhsyddNMMNNMMMmy-`             `-+ydNMMNNMm.    Please email  .   .   .\r\n"
-    ". . yy. .omh:+sdy+-`                    `.:yMMd.    admin@tempusmud.com\r\n"
-    ". . . . . /mm+``                          .yNs`    with any login issues   .\r\n"
-    ". . . . . .`oNm+.                       -sNh-\r\n"
-    ". . . . . . .`+dNh+.                `-odmy-    visit http://tempusmud.net  .\r\n"
-    ". . . . . . . . .ohmmhs+/--...-:/oydNds:`\r\n"
-    ". . . . . . . . . . ./+syhdmmddhyo/-`      .   .   .   .   .   .   .   .   .\r\n"
-    "\r\n"
-*/
 
 const char *WELC_MESSG =
     "\r\n" "Welcome to the realms of Tempus, adventurer.\r\n";
